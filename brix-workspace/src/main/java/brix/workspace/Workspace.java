2db202c058a0ae10e2b4cd7a77b5f34f4b2fc425
Matej Knopp <matej.knopp@gmail.com>
2008-07-02 00:53:24 +0000

    javadoc

--- brix-workspace/src/main/java/brix/workspace/Workspace.java
+++ brix-workspace/src/main/java/brix/workspace/Workspace.java
@@ -2,15 +2,48 @@ package brix.workspace;
 
 import java.util.Iterator;
 
-public interface Workspace
-{
-    public String getId();
-     
-    public void setAttribute(String attributeKey, String attributeValue);
-    
-    public String getAttribute(String attributeKey);
-    
-    public Iterator<String> getAttributeKeys();
-    
-    public void delete();
+/**
+ * Represents a single workspace. Allows to set and query workspace attributes.
+ * To obtain a workspace object use {@link WorkspaceManager}.
+ * 
+ * @author Matej Knopp
+ */
+public interface Workspace {
+
+	/**
+	 * Returns workspace id. Workspace id is a JCR workspace name.
+	 * 
+	 * @return
+	 */
+	public String getId();
+
+	/**
+	 * Sets the attribute with specified key to specified value. To remove
+	 * attribute use <code>null</code> value.
+	 * 
+	 * @param attributeKey
+	 * @param attributeValue
+	 */
+	public void setAttribute(String attributeKey, String attributeValue);
+
+	/**
+	 * Returns attribute with the specified key.
+	 * 
+	 * @param attributeKey
+	 * @return attribute value or <code>null</code> if such attribute is not
+	 *         present.
+	 */
+	public String getAttribute(String attributeKey);
+
+	/**
+	 * Returns all available attribute keys in this workspace.
+	 * 
+	 * @return
+	 */
+	public Iterator<String> getAttributeKeys();
+
+	/**
+	 * Deletes the workspace.
+	 */
+	public void delete();
 }
--- brix-workspace/src/main/java/brix/workspace/WorkspaceManager.java
+++ brix-workspace/src/main/java/brix/workspace/WorkspaceManager.java
@@ -3,15 +3,55 @@ package brix.workspace;
 import java.util.List;
 import java.util.Map;
 
-public interface WorkspaceManager
-{
-    public List<Workspace> getWorkspaces();
-    
-    public List<Workspace> getWorkspacesFiltered(Map<String, String> workspaceAttributes);    
-    
-    public Workspace createWorkspace();    
-    
-    public Workspace getWorkspace(String workspaceId);
-    
-    public boolean workspaceExists(String workspaceId);
+/**
+ * Workspace manager is a think layer on top of JCR extending the JCR workspace
+ * management functionality. It allows to delete workspaces, set workspace
+ * attributes and filter workspaces according to the workspaces.
+ * 
+ * @author Matej Knopp
+ */
+public interface WorkspaceManager {
+
+	/**
+	 * Returns the list of all available workspaces.
+	 * 
+	 * @return
+	 */
+	public List<Workspace> getWorkspaces();
+
+	/**
+	 * Returns a filtered list of workspaces. Each workspace in the resulting
+	 * list must have all specified workspace attributes set and the attributes
+	 * values must be equal to the values in the
+	 * <code>workspaceAttributes</code>map.
+	 * 
+	 * @param workspaceAttributes
+	 * @return
+	 */
+	public List<Workspace> getWorkspacesFiltered(
+			Map<String, String> workspaceAttributes);
+
+	/**
+	 * Creates a new workspace.
+	 * 
+	 * @return
+	 */
+	public Workspace createWorkspace();
+
+	/**
+	 * Returns the {@link Workspace} object associated with the workspace with
+	 * given id. The workspace id actually a JCR workspace name.
+	 * 
+	 * @param workspaceId
+	 * @return
+	 */
+	public Workspace getWorkspace(String workspaceId);
+
+	/**
+	 * Returns whether a workspace with given id exists.
+	 *  
+	 * @param workspaceId
+	 * @return
+	 */
+	public boolean workspaceExists(String workspaceId);
 }
