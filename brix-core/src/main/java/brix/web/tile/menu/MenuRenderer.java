330d90d3e326e293f37cee667594034ef8e46743
Matej Knopp <matej.knopp@gmail.com>
2008-07-03 00:28:34 +0000

    fix

--- brix-core/src/main/java/brix/web/tile/menu/MenuRenderer.java
+++ brix-core/src/main/java/brix/web/tile/menu/MenuRenderer.java
@@ -16,171 +16,169 @@ import brix.plugin.menu.Menu.ChildEntry;
 import brix.plugin.menu.Menu.Entry;
 import brix.web.generic.IGenericComponent;
 import brix.web.nodepage.BrixNodeWebPage;
+import brix.web.nodepage.BrixPageParameters;
 import brix.web.reference.Reference;
 import brix.web.reference.Reference.Type;
 
 public class MenuRenderer extends WebComponent implements IGenericComponent<BrixNode>
 {
 
-    public MenuRenderer(String id, IModel<BrixNode> model)
-    {
-        super(id, model);
-    }
+	public MenuRenderer(String id, IModel<BrixNode> model)
+	{
+		super(id, model);
+	}
 
-    @Override
-    protected void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag)
-    {
-        MenuContainer container = new MenuContainer();
-        container.load(getModelObject());
+	@Override
+	protected void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag)
+	{
+		MenuContainer container = new MenuContainer();
+		container.load(getModelObject());
 
-        Set<ChildEntry> selected = getSelectedItems(container.getMenu());
+		Set<ChildEntry> selected = getSelectedItems(container.getMenu());
 
-        Response response = getResponse();
-        renderEntry(container, container.getMenu().getRoot(), response, selected);
-    }
+		Response response = getResponse();
+		renderEntry(container, container.getMenu().getRoot(), response, selected);
+	}
 
-    private void renderEntry(MenuContainer container, Entry entry, Response response,
-            Set<ChildEntry> selected)
-    {
-        boolean outer = entry.getParent() == null;
+	private void renderEntry(MenuContainer container, Entry entry, Response response, Set<ChildEntry> selected)
+	{
+		boolean outer = entry.getParent() == null;
 
-        String klass = "";
-        if (outer && !Strings.isEmpty(container.getOuterContainerStyleClass()))
-        {
-            klass = " class='" + container.getOuterContainerStyleClass() + "'";
-        }
-        else if (!outer && !Strings.isEmpty(container.getInnerContainerStyleClass()))
-        {
-            klass = " class='" + container.getInnerContainerStyleClass() + "'";
-        }
+		String klass = "";
+		if (outer && !Strings.isEmpty(container.getOuterContainerStyleClass()))
+		{
+			klass = " class='" + container.getOuterContainerStyleClass() + "'";
+		}
+		else if (!outer && !Strings.isEmpty(container.getInnerContainerStyleClass()))
+		{
+			klass = " class='" + container.getInnerContainerStyleClass() + "'";
+		}
 
+		response.write("\n<ul");
+		response.write(klass);
+		response.write(">\n");
 
-        response.write("\n<ul");
-        response.write(klass);
-        response.write(">\n");
+		for (ChildEntry e : entry.getChildren())
+		{
+			renderChild(container, e, response, selected);
+		}
 
-        for (ChildEntry e : entry.getChildren())
-        {
-            renderChild(container, e, response, selected);
-        }
+		response.write("</ul>\n");
 
-        response.write("</ul>\n");
+	}
 
-    }
+	private void renderChild(MenuContainer container, ChildEntry entry, Response response, Set<ChildEntry> selectedSet)
+	{
+		boolean selected = selectedSet.contains(entry);
 
-    private void renderChild(MenuContainer container, ChildEntry entry, Response response,
-            Set<ChildEntry> selectedSet)
-    {
-        boolean selected = selectedSet.contains(entry);
+		String klass = "";
 
-        String klass = "";
+		if (selected && !Strings.isEmpty(container.getSelectedItemStyleClass()))
+		{
+			klass = container.getSelectedItemStyleClass();
+		}
 
-        if (selected && !Strings.isEmpty(container.getSelectedItemStyleClass()))
-        {
-            klass = container.getSelectedItemStyleClass();
-        }
+		if (!Strings.isEmpty(entry.getCssClass()))
+		{
+			if (!Strings.isEmpty(klass))
+			{
+				klass += " ";
+			}
+			klass += entry.getCssClass();
+		}
 
-        if (!Strings.isEmpty(entry.getCssClass()))
-        {
-            if (!Strings.isEmpty(klass))
-            {
-                klass += " ";
-            }
-            klass += entry.getCssClass();
-        }
+		response.write("\n<li");
 
-        response.write("\n<li");
+		if (!Strings.isEmpty(klass))
+		{
+			response.write(" class=\"");
+			response.write(klass);
+			response.write("\"");
+		}
 
-        if (!Strings.isEmpty(klass))
-        {
-            response.write(" class=\"");
-            response.write(klass);
-            response.write("\"");
-        }
+		response.write(">");
 
-        response.write(">");
+		final String url = entry.getReference() != null ? entry.getReference().generateUrl() : "#";
 
-        final String url = entry.getReference() != null ? entry.getReference().generateUrl() : "#";
+		response.write("<a href='");
+		response.write(url);
+		response.write("'>");
 
-        response.write("<a href='");
-        response.write(url);
-        response.write("'>");
+		// TODO. escape or not (probably a property would be nice?
 
-        // TODO. escape or not (probably a property would be nice?
+		response.write(entry.getTitle());
 
-        response.write(entry.getTitle());
+		response.write("</a>");
 
-        response.write("</a>");
+		if (selected && !entry.getChildren().isEmpty())
+		{
+			renderEntry(container, entry, response, selectedSet);
+		}
 
-        if (selected && !entry.getChildren().isEmpty())
-        {
-            renderEntry(container, entry, response, selectedSet);
-        }
+		response.write("</li>\n");
+	}
 
-        response.write("</li>\n");
-    }
+	private boolean isSelected(Reference reference, String url)
+	{
+		boolean eq = false;
 
-    private boolean isSelected(Reference reference, String url)
-    {
+		BrixNodeWebPage page = (BrixNodeWebPage) getPage();
 
-        boolean eq = false;
+		if (reference.getType() == Type.NODE)
+		{
+			eq = page.getModel().equals(reference.getNodeModel())
+					&& BrixPageParameters.equals(page.getBrixPageParameters(), reference.getParameters());
 
-        BrixNodeWebPage page = (BrixNodeWebPage)getPage();
+		}
+		else
+		{
+			eq = url.equals(reference.getUrl())
+					&& BrixPageParameters.equals(page.getBrixPageParameters(), reference.getParameters());
+		}
 
-        if (reference.getType() == Type.NODE)
-        {
-            eq = page.getModelObject().equals(reference.getNodeModel()) &&
-                page.getBrixPageParameters().equals(reference.getParameters());
-        }
-        else
-        {
-            eq = url.equals(reference.getUrl()) &&
-                page.getBrixPageParameters().equals(reference.getParameters());
-        }
+		return eq;
+	}
 
-        return eq;
-    }
+	private boolean isSelected(ChildEntry entry)
+	{
+		final String url = "/" + getRequest().getPath();
+		Reference ref = entry.getReference();
+		if (ref == null)
+		{
+			return false;
+		}
+		else
+		{
+			return isSelected(ref, url);
+		}
+	}
 
-    private boolean isSelected(ChildEntry entry)
-    {
-        final String url = "/" + getRequest().getPath();
-        Reference ref = entry.getReference();
-        if (ref == null)
-        {
-            return false;
-        }
-        else
-        {
-            return isSelected(ref, url);
-        }
-    }
+	private void checkSelected(ChildEntry entry, Set<ChildEntry> selectedSet)
+	{
+		if (isSelected(entry))
+		{
+			for (Entry e = entry; e instanceof ChildEntry; e = e.getParent())
+			{
+				selectedSet.add((ChildEntry) e);
+			}
+		}
+		for (ChildEntry e : entry.getChildren())
+		{
+			checkSelected(e, selectedSet);
+		}
+	}
 
-    private void checkSelected(ChildEntry entry, Set<ChildEntry> selectedSet)
-    {
-        if (isSelected(entry))
-        {
-            for (Entry e = entry; e instanceof ChildEntry; e = e.getParent())
-            {
-                selectedSet.add((ChildEntry)e);
-            }
-        }
-        for (ChildEntry e : entry.getChildren())
-        {
-            checkSelected(e, selectedSet);
-        }
-    }
+	Set<ChildEntry> getSelectedItems(Menu menu)
+	{
+		Set<ChildEntry> result = new HashSet<ChildEntry>();
 
-    Set<ChildEntry> getSelectedItems(Menu menu)
-    {
-        Set<ChildEntry> result = new HashSet<ChildEntry>();
+		for (ChildEntry e : menu.getRoot().getChildren())
+		{
+			checkSelected(e, result);
+		}
 
-        for (ChildEntry e : menu.getRoot().getChildren())
-        {
-            checkSelected(e, result);
-        }
-
-        return result;
-    }
+		return result;
+	}
 
 	@SuppressWarnings("unchecked")
 	public IModel<BrixNode> getModel()
