e3f438f796f74ed29469af7ed47c991c695f8e11
Igor Vaynberg <igor.vaynberg@gmail.com>
2008-07-10 17:06:54 +0000

    also add class to anchor inside selected li

--- brix-plugin-menu/src/main/java/brix/plugin/menu/tile/MenuRenderer.java
+++ brix-plugin-menu/src/main/java/brix/plugin/menu/tile/MenuRenderer.java
@@ -23,310 +23,336 @@ import brix.web.nodepage.BrixPageParameters;
 import brix.web.reference.Reference;
 import brix.web.reference.Reference.Type;
 
+/**
+ * Component used to render the menu
+ * 
+ * @author igor.vaynberg
+ * 
+ */
 public class MenuRenderer extends WebComponent implements IGenericComponent<BrixNode>
 {
+    private static final long serialVersionUID = 1L;
 
-	public MenuRenderer(String id, IModel<BrixNode> model)
-	{
-		super(id, model);
-	}
+    /**
+     * Constructor
+     * 
+     * @param id
+     * @param model
+     */
+    public MenuRenderer(String id, IModel<BrixNode> model)
+    {
+        super(id, model);
+    }
 
-	@Override
-	protected void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag)
-	{
-		MenuContainer container = new MenuContainer();
-		container.load(getModelObject());
+    /** {@inheritDoc} */
+    @Override
+    protected void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag)
+    {
+        MenuContainer container = new MenuContainer();
+        container.load(getModelObject());
 
-		Set<ChildEntry> selected = getSelectedItems(container.getMenu());
+        Set<ChildEntry> selected = getSelectedItems(container.getMenu());
 
-		// how many levels to skip to start rendering
-		int skipLevels = container.getStartAtLevel() != null ? container.getStartAtLevel() : 0;
+        // how many levels to skip to start rendering
+        int skipLevels = container.getStartAtLevel() != null ? container.getStartAtLevel() : 0;
 
-		// how many levels should be rendered
-		int renderLevels = container.getRenderLevels() != null ? container.getRenderLevels() : Integer.MAX_VALUE;
+        // how many levels should be rendered
+        int renderLevels = container.getRenderLevels() != null
+            ? container.getRenderLevels()
+            : Integer.MAX_VALUE;
 
-		Response response = getResponse();
-		renderEntry(container, container.getMenu().getRoot(), response, selected, skipLevels, renderLevels);
-	}
+        Response response = getResponse();
+        renderEntry(container, container.getMenu().getRoot(), response, selected, skipLevels,
+            renderLevels);
+    }
 
-	private void renderEntry(MenuContainer container, Entry entry, Response response, Set<ChildEntry> selected,
-			int skipLevels, int renderLevels)
-	{
-		if (renderLevels <= 0)
-		{
-			return;
-		}
+    private void renderEntry(MenuContainer container, Entry entry, Response response,
+            Set<ChildEntry> selected, int skipLevels, int renderLevels)
+    {
+        if (renderLevels <= 0)
+        {
+            return;
+        }
 
-		if (skipLevels <= 0)
-		{
-			boolean outer = skipLevels == 0;
-			String klass = "";
-			if (outer && !Strings.isEmpty(container.getOuterContainerStyleClass()))
-			{
-				klass = " class='" + container.getOuterContainerStyleClass() + "'";
-			}
-			else if (!outer && !Strings.isEmpty(container.getInnerContainerStyleClass()))
-			{
-				klass = " class='" + container.getInnerContainerStyleClass() + "'";
-			}
-			response.write("\n<ul");
-			response.write(klass);
-			response.write(">\n");
-		}
+        if (skipLevels <= 0)
+        {
+            boolean outer = skipLevels == 0;
+            String klass = "";
+            if (outer && !Strings.isEmpty(container.getOuterContainerStyleClass()))
+            {
+                klass = " class='" + container.getOuterContainerStyleClass() + "'";
+            }
+            else if (!outer && !Strings.isEmpty(container.getInnerContainerStyleClass()))
+            {
+                klass = " class='" + container.getInnerContainerStyleClass() + "'";
+            }
+            response.write("\n<ul");
+            response.write(klass);
+            response.write(">\n");
+        }
 
-		for (ChildEntry e : entry.getChildren())
-		{
-			BrixNode node = getNode(e);
-			if (node == null || SitePlugin.get().canViewNode(node, Context.PRESENTATION))
-			{
-				renderChild(container, e, response, selected, skipLevels, renderLevels);
-			}
-		}
+        for (ChildEntry e : entry.getChildren())
+        {
+            BrixNode node = getNode(e);
+            if (node == null || SitePlugin.get().canViewNode(node, Context.PRESENTATION))
+            {
+                renderChild(container, e, response, selected, skipLevels, renderLevels);
+            }
+        }
 
-		if (skipLevels <= 0)
-		{
-			response.write("</ul>\n");
-		}
-	}
+        if (skipLevels <= 0)
+        {
+            response.write("</ul>\n");
+        }
+    }
 
-	private BrixNode getNode(ChildEntry entry)
-	{
-		if (entry.getReference() != null && !entry.getReference().isEmpty()
-				&& entry.getReference().getType() == Type.NODE)
-		{
-			return entry.getReference().getNodeModel().getObject();
-		}
-		else
-		{
-			List<ChildEntry> children = entry.getChildren();
-			if (children != null && !children.isEmpty())
-			{
-				return getNode(children.iterator().next());
-			}
-			else
-			{
-				return null;
-			}
-		}
-	}
+    private BrixNode getNode(ChildEntry entry)
+    {
+        if (entry.getReference() != null && !entry.getReference().isEmpty() &&
+            entry.getReference().getType() == Type.NODE)
+        {
+            return entry.getReference().getNodeModel().getObject();
+        }
+        else
+        {
+            List<ChildEntry> children = entry.getChildren();
+            if (children != null && !children.isEmpty())
+            {
+                return getNode(children.iterator().next());
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
 
-	private String getUrl(ChildEntry entry)
-	{
-		if (entry.getReference() != null && !entry.getReference().isEmpty())
-		{
-			return entry.getReference().generateUrl();
-		}
-		else
-		{
-			List<ChildEntry> children = entry.getChildren();
-			if (children != null && !children.isEmpty())
-			{
-				return getUrl(children.iterator().next());
-			}
-			else
-			{
-				return "#";
-			}
-		}
-	}
+    private String getUrl(ChildEntry entry)
+    {
+        if (entry.getReference() != null && !entry.getReference().isEmpty())
+        {
+            return entry.getReference().generateUrl();
+        }
+        else
+        {
+            List<ChildEntry> children = entry.getChildren();
+            if (children != null && !children.isEmpty())
+            {
+                return getUrl(children.iterator().next());
+            }
+            else
+            {
+                return "#";
+            }
+        }
+    }
 
-	private boolean anyChildSelected(ChildEntry entry, Set<ChildEntry> selectedSet)
-	{
-		if (entry.getChildren() != null)
-		{
-			for (ChildEntry e : entry.getChildren())
-			{
-				if (selectedSet.contains(e))
-				{
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    private boolean anyChildSelected(ChildEntry entry, Set<ChildEntry> selectedSet)
+    {
+        if (entry.getChildren() != null)
+        {
+            for (ChildEntry e : entry.getChildren())
+            {
+                if (selectedSet.contains(e))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-	private void renderChild(MenuContainer container, ChildEntry entry, Response response, Set<ChildEntry> selectedSet,
-			int skipLevels, int renderLevels)
-	{
-		boolean selected = selectedSet.contains(entry);
+    private void renderChild(MenuContainer container, ChildEntry entry, Response response,
+            Set<ChildEntry> selectedSet, int skipLevels, int renderLevels)
+    {
+        boolean selected = selectedSet.contains(entry);
 
-		boolean anyChildren = selected && anyChildren(entry);
+        boolean anyChildren = selected && anyChildren(entry);
 
-		if (skipLevels <= 0)
-		{
-			String klass = "";
+        if (skipLevels <= 0)
+        {
+            String listItemCssClass = "";
+            String anchorCssClass = "";
+            if (selected && !Strings.isEmpty(container.getSelectedItemStyleClass()))
+            {
+                listItemCssClass = container.getSelectedItemStyleClass();
+                anchorCssClass = container.getSelectedItemStyleClass();
+            }
 
-			if (selected && !Strings.isEmpty(container.getSelectedItemStyleClass()))
-			{
-				klass = container.getSelectedItemStyleClass();
-			}
+            if (anyChildren && selected && anyChildSelected(entry, selectedSet) &&
+                !Strings.isEmpty(container.getItemWithSelectedChildStyleClass()))
+            {
+                listItemCssClass = container.getItemWithSelectedChildStyleClass();
+            }
 
-			if (anyChildren && selected && anyChildSelected(entry, selectedSet)
-					&& !Strings.isEmpty(container.getItemWithSelectedChildStyleClass()))
-			{
-				klass = container.getItemWithSelectedChildStyleClass();
-			}
+            if (!Strings.isEmpty(entry.getCssClass()))
+            {
+                if (!Strings.isEmpty(listItemCssClass))
+                {
+                    listItemCssClass += " ";
+                }
+                listItemCssClass += entry.getCssClass();
+            }
 
-			if (!Strings.isEmpty(entry.getCssClass()))
-			{
-				if (!Strings.isEmpty(klass))
-				{
-					klass += " ";
-				}
-				klass += entry.getCssClass();
-			}
+            response.write("\n<li");
 
-			response.write("\n<li");
+            if (!Strings.isEmpty(listItemCssClass))
+            {
+                response.write(" class=\"");
+                response.write(listItemCssClass);
+                response.write("\"");
+            }
 
-			if (!Strings.isEmpty(klass))
-			{
-				response.write(" class=\"");
-				response.write(klass);
-				response.write("\"");
-			}
+            response.write(">");
 
-			response.write(">");
+            final String url = getUrl(entry);
 
-			final String url = getUrl(entry);
+            response.write("<a");
+            if (!Strings.isEmpty(anchorCssClass))
+            {
+                response.write("class=\"");
+                response.write(anchorCssClass);
+                response.write("\"");
+            }
+            response.write(" href=\"");
+            response.write(url);
+            response.write("\"><span>");
 
-			response.write("<a href='");
-			response.write(url);
-			response.write("'><span>");
+            // TODO. escape or not (probably a property would be nice?
 
-			// TODO. escape or not (probably a property would be nice?
+            response.write(entry.getTitle());
 
-			response.write(entry.getTitle());
+            response.write("</span></a>");
+        }
 
-			response.write("</span></a>");
-		}
+        // only decrement skip levels for child if current is begger than 0
+        int childSkipLevels = skipLevels - 1;
 
-		// only decrement skip levels for child if current is begger than 0
-		int childSkipLevels = skipLevels - 1;
+        // only decrement render levels when we are already rendering
+        int childRenderLevels = skipLevels <= 0 ? renderLevels - 1 : renderLevels;
 
-		// only decrement render levels when we are already rendering
-		int childRenderLevels = skipLevels <= 0 ? renderLevels - 1 : renderLevels;
+        if (anyChildren)
+        {
+            renderEntry(container, entry, response, selectedSet, childSkipLevels, childRenderLevels);
+        }
 
-		if (anyChildren)
-		{
-			renderEntry(container, entry, response, selectedSet, childSkipLevels, childRenderLevels);
-		}
+        if (skipLevels == 0)
+        {
+            response.write("</li>\n");
+        }
+    }
 
-		if (skipLevels == 0)
-		{
-			response.write("</li>\n");
-		}
-	}
+    private boolean anyChildren(ChildEntry entry)
+    {
+        if (entry.getChildren() != null)
+        {
+            for (ChildEntry e : entry.getChildren())
+            {
+                BrixNode node = getNode(e);
+                if (node == null || SitePlugin.get().canViewNode(node, Context.PRESENTATION))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-	private boolean anyChildren(ChildEntry entry)
-	{
-		if (entry.getChildren() != null)
-		{
-			for (ChildEntry e : entry.getChildren())
-			{
-				BrixNode node = getNode(e);
-				if (node == null || SitePlugin.get().canViewNode(node, Context.PRESENTATION))
-				{
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    private boolean isSelected(Reference reference, String url)
+    {
+        boolean eq = false;
 
-	private boolean isSelected(Reference reference, String url)
-	{
-		boolean eq = false;
+        BrixNodeWebPage page = (BrixNodeWebPage)getPage();
 
-		BrixNodeWebPage page = (BrixNodeWebPage) getPage();
+        if (reference.getType() == Type.NODE)
+        {
+            eq = page.getModel().equals(reference.getNodeModel()) &&
+                comparePageParameters(page.getBrixPageParameters(), reference.getParameters());
 
-		if (reference.getType() == Type.NODE)
-		{
-			eq = page.getModel().equals(reference.getNodeModel())
-					&& comparePageParameters(page.getBrixPageParameters(), reference.getParameters());
+        }
+        else
+        {
+            eq = url.equals(reference.getUrl()) &&
+                comparePageParameters(page.getBrixPageParameters(), reference.getParameters());
+        }
 
-		}
-		else
-		{
-			eq = url.equals(reference.getUrl())
-					&& comparePageParameters(page.getBrixPageParameters(), reference.getParameters());
-		}
+        return eq;
+    }
 
-		return eq;
-	}
+    private boolean comparePageParameters(BrixPageParameters page, BrixPageParameters fromReference)
+    {
+        if (fromReference == null ||
+            (fromReference.getIndexedParamsCount() == 0 && fromReference.getQueryParamKeys()
+                .isEmpty()))
+        {
+            return true;
+        }
+        else
+        {
+            return BrixPageParameters.equals(page, fromReference);
+        }
+    }
 
-	private boolean comparePageParameters(BrixPageParameters page, BrixPageParameters fromReference)
-	{
-		if (fromReference == null
-				|| (fromReference.getIndexedParamsCount() == 0 && fromReference.getQueryParamKeys().isEmpty()))
-		{
-			return true;
-		}
-		else
-		{
-			return BrixPageParameters.equals(page, fromReference);
-		}
-	}
+    private boolean isSelected(ChildEntry entry)
+    {
+        final String url = "/" + getRequest().getPath();
+        Reference ref = entry.getReference();
+        if (ref == null)
+        {
+            return false;
+        }
+        else
+        {
+            return isSelected(ref, url);
+        }
+    }
 
-	private boolean isSelected(ChildEntry entry)
-	{
-		final String url = "/" + getRequest().getPath();
-		Reference ref = entry.getReference();
-		if (ref == null)
-		{
-			return false;
-		}
-		else
-		{
-			return isSelected(ref, url);
-		}
-	}
+    private void checkSelected(ChildEntry entry, Set<ChildEntry> selectedSet)
+    {
+        if (isSelected(entry))
+        {
+            for (Entry e = entry; e instanceof ChildEntry; e = e.getParent())
+            {
+                selectedSet.add((ChildEntry)e);
+            }
+        }
+        for (ChildEntry e : entry.getChildren())
+        {
+            checkSelected(e, selectedSet);
+        }
+    }
 
-	private void checkSelected(ChildEntry entry, Set<ChildEntry> selectedSet)
-	{
-		if (isSelected(entry))
-		{
-			for (Entry e = entry; e instanceof ChildEntry; e = e.getParent())
-			{
-				selectedSet.add((ChildEntry) e);
-			}
-		}
-		for (ChildEntry e : entry.getChildren())
-		{
-			checkSelected(e, selectedSet);
-		}
-	}
+    Set<ChildEntry> getSelectedItems(Menu menu)
+    {
+        Set<ChildEntry> result = new HashSet<ChildEntry>();
 
-	Set<ChildEntry> getSelectedItems(Menu menu)
-	{
-		Set<ChildEntry> result = new HashSet<ChildEntry>();
+        for (ChildEntry e : menu.getRoot().getChildren())
+        {
+            checkSelected(e, result);
+        }
 
-		for (ChildEntry e : menu.getRoot().getChildren())
-		{
-			checkSelected(e, result);
-		}
+        return result;
+    }
 
-		return result;
-	}
+    @SuppressWarnings("unchecked")
+    public IModel<BrixNode> getModel()
+    {
+        return (IModel<BrixNode>)getDefaultModel();
+    }
 
-	@SuppressWarnings("unchecked")
-	public IModel<BrixNode> getModel()
-	{
-		return (IModel<BrixNode>) getDefaultModel();
-	}
+    public BrixNode getModelObject()
+    {
+        return (BrixNode)getDefaultModelObject();
+    }
 
-	public BrixNode getModelObject()
-	{
-		return (BrixNode) getDefaultModelObject();
-	}
+    public void setModel(IModel<BrixNode> model)
+    {
+        setDefaultModel(model);
+    }
 
-	public void setModel(IModel<BrixNode> model)
-	{
-		setDefaultModel(model);
-	}
-
-	public void setModelObject(BrixNode object)
-	{
-		setDefaultModelObject(object);
-	}
+    public void setModelObject(BrixNode object)
+    {
+        setDefaultModelObject(object);
+    }
 
 }
