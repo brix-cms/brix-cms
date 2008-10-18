6ccd57dc9c4eea669c40965c4c633d07112f880c
Igor Vaynberg <igor.vaynberg@gmail.com>
2008-10-18 17:58:50 +0000

    Issue 34

--- brix-core/src/main/java/brix/web/nodepage/AbstractPageParametersLink.java
+++ brix-core/src/main/java/brix/web/nodepage/AbstractPageParametersLink.java
@@ -33,7 +33,7 @@ public abstract class AbstractPageParametersLink extends AbstractLink
             }
         });
         contributeToPageParameters(parameters);
-        return parameters.toCallbackURL();
+        return constructUrl(parameters);
     }
 
     /**
